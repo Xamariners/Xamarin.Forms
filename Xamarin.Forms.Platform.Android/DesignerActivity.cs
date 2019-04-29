b1fde661ff392aeb57eaa33826a517abf2223a1b
ShaneN <shane94@hotmail.com>
2019-04-29 11:14:58 -0600

    shell

---
+++ Xamarin.Forms.Platform.Android/DesignerActivity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace Xamarin.Forms.Platform.Android
+{
+	public class DesignerActivity : AppCompatActivity
+	{
+		public DesignerActivity()
+		{
+		}
+
+		protected DesignerActivity(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+		{
+			
+		}
+	}
+}
